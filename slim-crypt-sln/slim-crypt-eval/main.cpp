938a99f62e2abb297b7455108ef635c70f83e604
Michał Granda <majcoch98@gmail.com>
2021-05-27 11:06:48 +0200

    Implement test for Blowfish initialization

--- slim-crypt-sln/slim-crypt-eval/main.cpp
+++ slim-crypt-sln/slim-crypt-eval/main.cpp
@@ -12,20 +12,25 @@ int main(int argc, char* argv[]) {
 	uint8_t message[] = "This is a message we will encrypt with AES!";
 
 	/* SH-1 hash example */
+	// Verified
 	uint32_t hash[5] = { 0 };
-	sha1_hash(message, 32, hash);
+	sha1_hash(message, strlen((char*)message) * 8, hash);
 
 	/* AES encrypting and decrypting example */
 	aes_128_context_t aes = { { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 }, { 0 } };
 	aes_128_init(&aes);
+	// Message must be multiple of 16 bytes, parametr in bytes
+	// Verified
 	aes_128_encrypt(&aes, message, 32);
 	aes_128_decrypt(&aes, message, 32);
 
 	/* DES encrypting and decrypting example */
 	des_context_t des = { 0xAABB09182736CCDD, {0} };
 	des_init(&des);
-	des_encrypt(&des, message, 8);
-	des_decrypt(&des, message, 8);
+	// Message must be multiple of 8, parameter in bytes
+	// Verified
+	des_encrypt(&des, message, 32);
+	des_decrypt(&des, message, 32);
 
 	/* TEA encrypting and decrypting example */
 	tea_context_t tea = { {0x12, 0x65, 0x22, 0x55} };
@@ -33,6 +38,10 @@ int main(int argc, char* argv[]) {
 	tea_decrypt(&tea, message, 8);
 
 	/* Blowfish encrypting and decrypting example */
+	// Message must be multiple of 8, parameter in bytes
+	// Verified
+	uint32_t key[] = { 0x01, 0x02 };
+	blowfish_init(key, 2);
 	blowfish_encrypt(message, 8);
 	blowfish_decrypt(message, 8);
 
